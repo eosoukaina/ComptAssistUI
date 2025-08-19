a1fd8d10d90eeb7ed0dcd676f102ed408edc2409
elhad <soukaina.elhadifi@etu.uae.ma>
2025-08-19 21:11:17 +0100

    redesigning the entry page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { NbLayoutModule, NbButtonModule } from '@nebular/theme';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NbButtonModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    NbLayoutModule,
-    NbButtonModule,
-    RouterModule
+    CommonModule, 
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive,
+    NbButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'comptassistUI';
+  title = 'ComptAssist';
 }
